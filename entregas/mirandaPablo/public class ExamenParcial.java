95d9777bd57d39d7bf236472328a2f5d46563d19
pablomiraandaa <114480602+pablomiraandaa@users.noreply.github.com>
2022-11-22 13:39:36 +0100

    Add files via upload

---
+++ entregas/mirandaPablo/public class ExamenParcial.java
@@ -0,0 +1,49 @@
+public class ExamenParcial {
+    public static void main(String[]args){
+        int agujero1=1;
+        int agujero2=2;
+        int agujero3=4;
+        int agujero4=6;
+        int jugador=0;
+        int pelota;
+        int numero1;
+
+        Scanner entrada =  new Scanner(System.in);
+        System.out.println("Dime un número del 1 al 4 ");
+        int.class.math((agujero1*100)+60);
+        int.class.math((agujero2*100)+40);
+        int.class.math((agujero3*100)+30);
+        int.class.math((agujero4*100)+10);
+        do{
+             if{
+                numero=1;
+                jugador ++;
+                System.out.println("Usted ha avanzado 1 casilla ");
+                }
+            if{
+                    numero=2;
+                    jugador= +2;
+                    System.out.println("Usted ha avanzado 2 casillas ");
+                }
+             if{
+                    numero=3;
+                    jugador= +4;
+                    System.out.println("Usted ha avanzado 4 casillas ");
+                }
+             if{
+                    numero=4;
+                    jugador= +6;
+                    System.out.println("Usted ha avanzado 6 casillas ");
+                }
+        }
+        while{
+            jugador<(60);
+        }
+        if{
+            jugador=60;
+            System.out.println("Usted ha ganado ");
+        }
+
+    }
+    
+}
