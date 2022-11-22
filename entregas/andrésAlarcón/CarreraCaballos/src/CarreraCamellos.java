77e0a391cd6c39ac1bc931d23490bfb11d3aaa31
AndrextA <andrextalarcon@gmail.com>
2022-11-22 14:05:40 +0100

    Update

---
+++ entregas/andrésAlarcón/CarreraCaballos/src/CarreraCamellos.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class CarreraCamellos {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        int turnos = 0; 
+        int turnosMax = 50;
+        int posicion = 0;
+        int intentos = 0;
+        int fallos = 0;
+        int posicionCasillas = 0;
+
+        int prob1 = 60, prob2 = 40, prob3 = 30, prob4 = 10;
+
+
+        double agujeroAciertoProbabilidad = Math.random();
+
+        String vallaPistaSuperior = "---+-----------+---------+---------+---------+---------+-----------+";
+        String vallaPistaInferior = "---+-----------+---------+---------+---------+---------+-----------+";
+        String pistaNumeroCasillasSimplificado = "---| 0.........1.........2.........3.........4.........5.........6 |";
+        String pistaNumeroCasillas = "---| 0123456789012345678901234567890123456789012345678901234567890 |";
+        String camello = "[J]| ;--;'";
+
+        do {
+
+
+
+        // Display de la carrera
+
+        System.out.println(vallaPistaSuperior);
+        System.out.println(pistaNumeroCasillasSimplificado);
+        System.out.println(pistaNumeroCasillas);
+        System.out.println(camello);
+        System.out.println(vallaPistaInferior);
+
+        // Display de acciones
+
+        System.out.print("Turno ");
+        System.out.print(turnos);
+        System.out.print(" Avanza " + posicion);
+        System.out.println(" Estas en la casilla " + posicionCasillas);
+
+        int agujeroEscogido;
+        System.out.println("Elija un agujero:");
+        agujeroEscogido = sc.nextInt();
+
+            if (agujeroEscogido == 1) {
+
+                posicionCasillas = posicionCasillas +1;
+                posicion = +1;
+                turnos = turnos +1;
+
+            }
+
+            if (agujeroEscogido == 2) {
+
+                posicionCasillas = posicionCasillas +2;
+                posicion =  +2;
+                turnos = turnos +1;
+
+            }
+
+            if (agujeroEscogido == 3) {
+
+                posicionCasillas = posicionCasillas +3;
+                posicion = +3;
+                turnos = turnos +1;
+
+            }
+
+            if (agujeroEscogido == 4) {
+
+                posicionCasillas = posicionCasillas +4;
+                posicion = +4;
+                turnos = turnos +1;
+
+            }
+        
+        
+            if (posicionCasillas == 60) {
+
+                System.out.println("HAZ GANADO!!");
+            }
+ 
+        } while (posicionCasillas < 60);
+
+
+
+        sc.close();
+
+    }
+
+}
